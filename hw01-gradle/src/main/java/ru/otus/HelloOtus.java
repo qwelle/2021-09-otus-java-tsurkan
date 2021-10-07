d8435e10da88daca90d4d802708a67a70e567c5b
Marina Tsurkan <qwell91@mail.ru>
2021-10-07 22:03:45 +0300

    принципы работы gradle

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,8 @@
+package ru.otus;
+import com.google.common.math.DoubleMath;
+
+public class HelloOtus {
+    public static void main(String... args) {
+        System.out.println("5!: " + DoubleMath.factorial(5));
+    }
+}
