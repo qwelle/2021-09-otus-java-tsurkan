3500d4478d54ae64f55fdae9fbeec22e8793c01b
Marina Tsurkan <qwell91@mail.ru>
2021-11-06 14:09:26 +0300

    исходники для дз

---
+++ hw08-gc/src/main/java/homework/Data.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Data {
+    private final Integer value;
+
+    public Data(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
