4b7c44b53a17375a5de786bc062aca32351adc64
Marina Tsurkan <qwell91@mail.ru>
2021-11-09 20:43:18 +0300

    оптимизация

--- hw08-gc/src/main/java/homework/Data.java
+++ hw08-gc/src/main/java/homework/Data.java
@@ -1,13 +1,13 @@
 package homework;
 
 public class Data {
-    private final Integer value;
+    private final int value;
 
-    public Data(Integer value) {
+    public Data(int value) {
         this.value = value;
     }
 
-    public Integer getValue() {
+    public int getValue() {
         return value;
     }
 }
