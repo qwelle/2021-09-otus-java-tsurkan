9112bf1bf2a6bd3689bb4993a0f30144df1fbbc3
Marina Tsurkan <qwell91@mail.ru>
2021-11-24 21:37:12 +0300

    правка: вызов рефлексии один раз

--- hw10-aop/src/main/java/homework/MyProxy.java
+++ hw10-aop/src/main/java/homework/MyProxy.java
@@ -3,7 +3,7 @@ package homework;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 class MyProxy {
 
@@ -14,6 +14,20 @@ class MyProxy {
 
     static class LogInvocationHandler implements InvocationHandler {
         private final LogInterface logIntrfc;
+        private static final ArrayList<String> listLogMethods = new ArrayList<>();
+
+        static {
+            Class<?> clazz = LogImpl.class;
+            Method[] methods = clazz.getDeclaredMethods();
+            for (Method m : methods) {
+                String methodClass = m.toString();
+                String methodClassShort = methodClass.substring(methodClass.lastIndexOf('.') + 1);
+                //если у метода класса есть аннотация @Log, то сохраняем метод в коллекцию
+                if (m.isAnnotationPresent(Log.class)) {
+                    listLogMethods.add(methodClassShort);
+                }
+            }
+        }
 
         LogInvocationHandler(LogInterface logIntrfc) {
             this.logIntrfc = logIntrfc;
@@ -23,25 +37,13 @@ class MyProxy {
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             String methodInterface = method.toString();
             String methodInterfaceShort = methodInterface.substring(methodInterface.lastIndexOf('.') + 1);
-
-            Class<?> clazz = LogImpl.class;
-            Method[] methods = clazz.getDeclaredMethods();
-
-            for(Method m : methods) {
-                String methodClass = m.toString();
-                String methodClassShort = methodClass.substring(methodClass.lastIndexOf('.') + 1);
-
-                //сравниваем метод интерфейса с методами класса по текстовой сигнатуре
-                if (methodClassShort.equals(methodInterfaceShort)) {
-                    //если у метода класса есть аннотация Log, то
-                    String params = "";
-                    if (m.isAnnotationPresent(Log.class)) {
-                        for (int i = 0; i < args.length; i++) {
-                            params = params.concat(args[i] + " ");
-                        }
-                        System.out.println("executed method: " + method.getName() + ", param: " + params);
-                    }
+            //если метод интерфейса есть в списке методов класса с логированием
+            if (listLogMethods.contains(methodInterfaceShort)) {
+                String params = "";
+                for (int i = 0; i < args.length; i++) {
+                    params = params.concat(args[i] + " ");
                 }
+                System.out.println("executed method: " + method.getName() + ", param: " + params);
             }
             //для всех методов
             return method.invoke(logIntrfc, args);
